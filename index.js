3d6d418c7c284a06c2fcf5876d2ae43c349c905f
j-pedro-faria-alb <j-pedro-faria@alticelabs.com>
2025-05-23 13:12:54 +0000

    corrected endpoint

--- index.js
+++ index.js
@@ -6,22 +6,6 @@ app.get('/', (req, res) => {
   res.send('Vaquinet API is running!');
 });
 
-app.get('/cows', (req, res) => {
-  res.json([
-    {
-      id: '1',
-      name: 'Luna',
-      temperature: 38.5,
-      location: 'Field A'
-    },
-    {
-      id: '2',
-      name: 'Bella',
-      temperature: 39.0,
-      location: 'Field B'
-    }
-  ]);
-});
 app.get('/cows', async (req, res) => {
   const { data, error } = await supabase
     .from('cows')
